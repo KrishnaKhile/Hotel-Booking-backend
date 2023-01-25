784a51abb225d00eb57c7d9a950ba03df615da99
KrishnaKhile <103939308+KrishnaKhile@users.noreply.github.com>
2023-01-25 20:55:25 +0530

    cors

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ mongoose.connection.on("connected", () => {
 // middleware
 app.use(cors(
   {
-    origin: 'https://trps.netlify.app',
+    origin: '*',
     credentials: true,
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
   }
